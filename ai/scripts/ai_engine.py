5c0e8d857537fb0ef7347de6e79dbfd657e7f62a
Matrix25-prog <ambix25@gmail.com>
2025-04-13 16:43:03 -0400

    Update ai_engine.py

--- ai/scripts/ai_engine.py
+++ ai/scripts/ai_engine.py
@@ -1,8 +1,14 @@
 import openai
+from dotenv import load_dotenv
+import os
 from scripts.generate_prompt import build_questionnaire_prompt
 
 
-openai.api_key = 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
+load_dotenv()
+
+
+openai.api_key = os.getenv(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
+)
 
 def get_recommendation_from_questionnaire():
     prompt = build_questionnaire_prompt()
