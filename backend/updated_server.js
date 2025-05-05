db18a86dcb3caa6689ad003a8f68848df22f6cd0
Matrix25-prog <ambix25@gmail.com>
2025-05-04 22:40:13 -0400

    Update updated_server.js

--- backend/updated_server.js
+++ backend/updated_server.js
@@ -88,6 +88,6 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Backend server running on http://localhost:${PORT}`);
+  console.log(` Backend server running on http://localhost:${PORT}`);
 });
 
